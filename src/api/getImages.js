fd1e93736e9694b87d46958d1455c47699564429
Artem Zymovets <artwayprojects@gmail.com>
2021-01-07 21:06:02 +0200

    https agent

---
+++ src/api/getImages.js
@@ -0,0 +1,31 @@
+import https from 'https';
+import axios from 'axios';
+const API_KEY = '19741747-aaef32235b2696c68d5824b79';
+const BASE_URL = 'https://pixabay.com/api';
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
+const getImages = async (q, page = 1) => {
+  try {
+    const { data } = await axios.get(
+      '',
+      { params: { q, page } },
+      {
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      },
+    );
+    return data.hits;
+  } catch (error) {
+    console.log('error', { error });
+    return [];
+  }
+};
+
+export default getImages;
