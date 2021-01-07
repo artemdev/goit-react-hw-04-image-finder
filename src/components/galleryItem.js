987e185a336e257490371206e643c651c6607666
Artem Zymovets <artwayprojects@gmail.com>
2021-01-07 13:13:33 +0200

    modal window

--- src/components/galleryItem.js
+++ src/components/galleryItem.js
@@ -1,28 +1,60 @@
-import { Component } from 'react'
-import * as basicLightbox from 'basiclightbox'
+import { Component } from 'react';
+import * as basicLightbox from 'basiclightbox';
+
+//close modal on Esc button
+
 class GalleryItem extends Component {
-    openModal = (image) => {
-        const instance = basicLightbox.create(`
+  toggleModal = image => {
+    const closeModalWithEscape = e => {
+      if (e.key === 'Escape') this.modalInstance.close();
+    };
+    const closeModal = () => {
+      this.modalInstance.close();
+    };
+
+    this.modalInstance = basicLightbox.create(
+      `
           <div class="Overlay">
             <div class="Modal">
                 <img src=${image.largeImageURL} alt="" />
             </div>
         </div>
-        `)
+        `,
+      {
+        closable: true,
+        onShow: () => {
+          document.addEventListener('keydown', closeModalWithEscape);
+          // close modal by click on Overlay
+          setTimeout(() => {
+            document
+              .querySelector('.Overlay')
+              .addEventListener('click', closeModal);
+          }, 500);
+          // setTimeconsole.log()
+        },
+        onClose: () => {
+          document.removeEventListener('keydown', closeModalWithEscape);
+          document
+            .querySelector('.Overlay')
+            .removeEventListener('click', closeModal);
+        },
+      },
+    );
+    this.modalInstance.show();
+  };
 
-        instance.show()
-    }
-    render() {
-        const { image } = this.props
+  render() {
+    const { image } = this.props;
 
-
-        return (
-            <img onClick={() => this.openModal(image)} src={image.webformatURL} alt="" className="ImageGalleryItem-image" />
-
-        )
-    }
-
-
+    return (
+      <img
+        onClick={() => this.toggleModal(image)}
+        src={image.webformatURL}
+        alt=""
+        className="ImageGalleryItem-image"
+      />
+    );
+  }
 }
 
-export default GalleryItem
+export default GalleryItem;
